697e0e05d6d02a55c628eb6668228dad422c13ab
mussu777 <mustak65ee@gmail.com>
2025-05-17 00:24:35 +0530

    Combined client and server into one repo

---
+++ client/App.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Navigation from '@navigation/Navigation';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+
+const App = () => {
+   return (
+     <GestureHandlerRootView style={{flex: 1}}>
+       <Navigation />
+     </GestureHandlerRootView>
+   );
+};
+
+export default App;
